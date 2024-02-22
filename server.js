76540436ca2dc065c8bb8d03d07d1d64d585ec86
Temhithorphe <Tehmhithorphe@gmail.com>
2024-02-22 23:01:59 +0100

    [Major Update]: - Added API authentication & authorization - Updated users table schema. - Added API endpoints for updating of mining info. - Updated SQLite DB name (due to schema upgrade).

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sqlite3 = require("sqlite3");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const assert = require('assert');
+const { generateRandomHex } = require("./util");
 
 
 // ===================== RENDER PATH SETUP STARTS HERE ======================
@@ -15,7 +16,7 @@ const IS_ON_PROD = ENV_VALUE === 'false' ? false : true;
 
 // Setup "Render" persisted disk directory path and path_to_sqlite_db.
 const PERSISTENT_DISK_ROOT = "/var/lib/data/";
-const PATH_TO_SQLITE_DB = IS_ON_PROD ? `${PERSISTENT_DISK_ROOT}prod-crypto-miner.db` : "crypto-miner.db";
+const PATH_TO_SQLITE_DB = IS_ON_PROD ? `${PERSISTENT_DISK_ROOT}prod-crypto-miner-v2.db` : "crypto-miner.db";
 // console.log(PATH_TO_SQLITE_DB);
 
 // ===================== RENDER PATH SETUP ENDS HERE ======================
@@ -31,6 +32,7 @@ app.set("views", `${__dirname}/views`);
 
 
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 app.use(
   cors({
     origin: "*", // Add your frontend URL
@@ -39,6 +41,9 @@ app.use(
   })
 );
 
+// Determine the platform-specific extension
+// const extension = process.platform === 'win32' ? 'dll' : 'so';
+
 
 // Use a persistent SQLite database instead of in-memory
 const db = new sqlite3.Database(PATH_TO_SQLITE_DB, (err) => {
@@ -46,13 +51,24 @@ const db = new sqlite3.Database(PATH_TO_SQLITE_DB, (err) => {
     console.error("Error opening database:", err.message);
   } else {
     console.log("Connected to the SQLite database.");
+
+    // Enable the JSON1 extension
+    // db.run(`SELECT load_extension("libsqlite3_json.${extension}")`);
+
     // Create the users table if it does not exist
     db.run(
-      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT, approved BOOLEAN DEFAULT 0, allow_withdraw BOOLEAN NOT NULL DEFAULT 0 )"
+      `CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
+        password TEXT,
+        approved BOOLEAN DEFAULT 0,
+        allow_withdraw BOOLEAN NOT NULL DEFAULT 0,
+        mining_info TEXT NOT NULL DEFAULT '{"aumCount":0,"gipCount":0,"longCount":0,"totalVolume":0}',
+        api_token TEXT
+        )`
     );
   }
 });
-
 
 
 
@@ -135,6 +151,78 @@ app.post("/admin/approve/:userId", (req, res) => {
 
 
 
+// =============================== USER-FACING API CALLS START HERE =====================================>>
+
+// (Should be GET, but since no AUTHTOKEN setup, let's prevent simple GET request).
+// Retrieve user's mining status.
+app.post("/api/user/:userEmail/mining", (req, res) => {
+  const userEmail = req.params.userEmail;
+
+  // Retrieve mining info for user.
+  db.get("SELECT * FROM users WHERE email = ?", [userEmail], (err, row) => {
+    if (err) {
+      console.log("[DB - ERROR]: ", err);
+      return res.status(500).json({ error: "Something Wrong, Try again." });
+    }
+    console.log("[ROW]: ", row);
+
+    // If user is not yet approved.
+    if (parseInt(row.approved) !== 1) {
+      return res.status(400).json({
+        error: "This User needs Admin approval!"
+      });
+    }
+
+    const mining_info = JSON.parse(row.mining_info);
+    res.status(200).json({
+      mining_info,
+    });
+
+  })
+});
+
+
+
+// Update user's mining status.
+app.put("/api/user/:userEmail/mining", (req, res) => {
+  const userEmail = req.params.userEmail;
+
+  let { miningInfo } = req.body;
+  const userToken = req.headers['user-api-token'];
+
+  // console.log("[UPDATE]: ", userEmail, userToken, miningInfo);
+
+  db.get('SELECT api_token FROM users WHERE api_token = ? AND email = ?', userToken, userEmail, function (err, row) {
+    if (err) {
+      console.log("[DB - ERROR]: ", err);
+      return res.status(500).json({ error: "Something Wrong, Try again." });
+    }
+
+    // No user found for the specified criteria.
+    if (!row) return res.status(403).json({ error: "Unauthorized Access, Try again." });
+
+    // Insert JSON data into the table. 
+    db.run("UPDATE users SET mining_info = ? WHERE api_token = ? AND email = ?", miningInfo, userToken, userEmail, (err) => {
+      if (err) {
+        console.log("[DB - ERROR]: ", err);
+        return res.status(500).json({ error: "Something Wrong With Update, Try again." });
+      }
+
+      // User record updated.
+      // const mining_info = JSON.parse(row.mining_info);
+      res.status(200).json({
+        msg: "Record Updated"
+      });
+
+    });
+
+  })
+
+});
+
+
+
+
 app.post("/api/withdrawal/approval/:userEmail", (req, res) => {
   const userEmail = req.params.userEmail;
 
@@ -150,6 +238,7 @@ app.post("/api/withdrawal/approval/:userEmail", (req, res) => {
       withdrawal_status: status,
       msg: status ? 'Withdrawal Approved' : "User Not VIP Approved",
     });
+
   })
 });
 
@@ -158,9 +247,15 @@ app.post("/api/withdrawal/approval/:userEmail", (req, res) => {
 app.post("/api/auth", (req, res) => {
   const { email, password } = req.body;
 
-  // Check if the user already exists
+  if (!email || !password) return res.status(400).json({ error: "Credentials Not Valid . . . Try again with valid creds." })
+
+  const userAPIToken = generateRandomHex();
+  // console.log("[USER-REG]:", email, password, userAPIToken);
+
+  // Check if the user already exists.
   db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
     if (err) {
+      console.log("[DB-ERROR]:", err);
       return res.status(500).json({ error: "Internal server error" });
     }
 
@@ -169,12 +264,15 @@ app.post("/api/auth", (req, res) => {
       if (row.approved) {
         // User is approved, perform login
         db.get(
-          "SELECT id, email FROM users WHERE email = ? AND password = ?",
+          "SELECT * FROM users WHERE email = ? AND password = ?",
           [email, password],
           (loginErr, loginRow) => {
             if (loginErr) {
+              console.log("[DB-ERROR]:", loginErr);
               return res.status(500).json({ error: "Internal server error" });
             }
+
+            // console.log("[LOGIN]: ", loginRow);
 
             if (!loginRow) {
               return res.status(401).json({ error: "Invalid credentials" });
@@ -182,7 +280,7 @@ app.post("/api/auth", (req, res) => {
 
             return res.status(200).json({
               message: "Login successful",
-              user: { id: loginRow.id, email: loginRow.email },
+              user: { id: loginRow.id, email: loginRow.email, token: loginRow.api_token, miningInfo: JSON.parse(loginRow.mining_info) }
             });
           }
         );
@@ -193,10 +291,11 @@ app.post("/api/auth", (req, res) => {
     } else {
       // User doesn't exist, perform registration
       db.run(
-        "INSERT INTO users (email, password) VALUES (?, ?)",
-        [email, password],
+        "INSERT INTO users (email, password, api_token) VALUES (?, ?, ?)",
+        [email, password, userAPIToken],
         function (registerErr) {
           if (registerErr) {
+            console.log("[DB-ERROR]:", registerErr);
             return res.status(500).json({ error: "Failed to register user" });
           }
 
@@ -204,13 +303,17 @@ app.post("/api/auth", (req, res) => {
 
           return res.status(200).json({
             message: "User registered successfully click again to login",
-            user: { id: userId, email },
+            user: { id: userId, email, token: null },
           });
         }
       );
     }
   });
 });
+
+
+// =============================== USER-FACING API CALLS ENDS HERE =====================================<<
+
 
 
 app.listen(PORT, () => {
