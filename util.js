df91abdf6d4da5a813c832775ddf20da7278fb24
TEMHITHORPHE <TEHMHITHORPHE@GMAIL.COM>
2024-07-26 08:14:45 +0100

    fix now allows specifying length

--- util.js
+++ util.js
@@ -1,10 +1,10 @@
 const crypto = require('node:crypto');
 
-function generateRandomHex() {
+function generateRandomHex(size = 12) {
     const buffer = Buffer.allocUnsafe(6); // 6 bytes for a 48-bit integer
     crypto.randomFillSync(buffer);
 
-    const randomHex = buffer.toString('hex').toUpperCase().slice(0, 12);
+    const randomHex = buffer.toString('hex').toUpperCase().slice(0, size);
     return randomHex;
 };
 
