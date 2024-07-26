ab568f1df8c47581c22fe339ffeeb5403661fe15
TEMHITHORPHE <TEHMHITHORPHE@GMAIL.COM>
2024-07-26 08:16:20 +0100

    major upgrade for license use.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const IS_ON_PROD = ENV_VALUE === 'false' ? false : true;
 // Setup "Render" persisted disk directory path and path_to_sqlite_db.
 const PERSISTENT_DISK_ROOT = "/var/lib/data/";
 const PATH_TO_SQLITE_DB = IS_ON_PROD ? `${PERSISTENT_DISK_ROOT}prod-crypto-miner-v2.db` : "crypto-miner.db";
-// console.log(PATH_TO_SQLITE_DB);
+console.log(PATH_TO_SQLITE_DB);
 
 // ===================== RENDER PATH SETUP ENDS HERE ======================
 
@@ -59,8 +59,8 @@ const db = new sqlite3.Database(PATH_TO_SQLITE_DB, (err) => {
     db.run(
       `CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE,
-        password TEXT,
+        license_key TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
         approved BOOLEAN DEFAULT 0,
         allow_withdraw BOOLEAN NOT NULL DEFAULT 0,
         mining_info TEXT NOT NULL DEFAULT '{"aumCount":0,"gipCount":0,"longCount":0,"totalVolume":0}',
@@ -89,7 +89,7 @@ app.post("/admin/revoke/withdrawal/:userId", (req, res) => {
   const userId = req.params.userId;
   console.log(userId);
 
-  // Update the user's 'withdrawal' status in the database
+  // Update the user's  'withdrawal' status in the database
   db.run(
     "UPDATE users SET allow_withdraw = 0 WHERE id = ?",
     [userId],
@@ -152,8 +152,8 @@ app.post("/admin/approve/:userId", (req, res) => {
 
 
 // Endpoint to delete a user by ID
-app.delete('/admin/hidden/:accessCode/user/:userEmail', (req, res) => {
-  const userEmail = req.params.userEmail;
+app.delete('/admin/hidden/:accessCode/user/:userId', (req, res) => {
+  const userId = req.params.userId;
 
   const d = new Date;
   const currentCode = d.getDate().toString() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
@@ -161,14 +161,14 @@ app.delete('/admin/hidden/:accessCode/user/:userEmail', (req, res) => {
   if (req.params.accessCode.trim() !== currentCode) return res.status(403).json({ error: 'Unauthorized Operation Access' });
 
   // Perform the deletion
-  db.run('DELETE FROM users WHERE email = ?', userEmail, function (err) {
+  db.run('DELETE FROM users WHERE id = ?', userId, function (err) {
     if (err) {
       console.error(err.message);
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
 
-    // Check the number of affected rows
+    // Check the number of affected rows.
     if (this.changes === 0) {
       res.status(404).json({ error: 'User not found' });
     } else {
@@ -179,15 +179,51 @@ app.delete('/admin/hidden/:accessCode/user/:userEmail', (req, res) => {
 
 
 
+app.post("/admin/:accessCode/create/license", (req, res) => {
+  /* 
+    Setup a dynamic accesscode to prevent easy unauthorized access (this is stupid securIty btw).
+  */
+  const d = new Date;
+  const currentCode = d.getDate().toString() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+  if (req.params.accessCode.trim() !== currentCode) return res.status(403).json({ error: 'Unauthorized Operation Access' });
+
+  const { email } = req.body;
+  if (email === '') return res.status(400).json({ error: 'Email not provided/invalid' });
+
+  const userAPIToken = generateRandomHex();
+  const licenseKey = generateRandomHex(10);
+
+  db.run(
+    "INSERT INTO users (email, license_key, api_token) VALUES (?, ?, ?)",
+    [email, licenseKey, userAPIToken],
+    function (registerErr) {
+      if (registerErr) {
+        console.log("[DB-ERROR]:", registerErr);
+        return res.status(500).json({ error: "Failed to create license key" });
+      }
+      
+      const userId = this.lastID; // Get the last inserted row ID
+
+      return res.status(200).json({
+        message: `License Key ${licenseKey} Created`,
+        // user: { id: userId, email, token: null },
+      });
+    }
+  );
+})
+
+
+
+
 // =============================== USER-FACING API CALLS START HERE =====================================>>
 
 // (Should be GET, but since no AUTHTOKEN setup, let's prevent simple GET request).
 // Retrieve user's mining status.
-app.post("/api/user/:userEmail/mining", (req, res) => {
-  const userEmail = req.params.userEmail;
+app.post("/api/user/:userId/mining", (req, res) => {
+  const userId = req.params.userId;
 
   // Retrieve mining info for user.
-  db.get("SELECT * FROM users WHERE email = ?", [userEmail], (err, row) => {
+  db.get("SELECT * FROM users WHERE id = ?", [userId], (err, row) => {
     if (err) {
       console.log("[DB - ERROR]: ", err);
       return res.status(500).json({ error: "Something Wrong, Try again." });
@@ -212,15 +248,15 @@ app.post("/api/user/:userEmail/mining", (req, res) => {
 
 
 // Update user's mining status.
-app.put("/api/user/:userEmail/mining", (req, res) => {
-  const userEmail = req.params.userEmail;
+app.put("/api/user/:userId/mining", (req, res) => {
+  const userId = req.params.userId;
 
   let { miningInfo } = req.body;
   const userToken = req.headers['user-api-token'];
 
   // console.log("[UPDATE]: ", userEmail, userToken, miningInfo);
 
-  db.get('SELECT api_token FROM users WHERE api_token = ? AND email = ?', userToken, userEmail, function (err, row) {
+  db.get('SELECT api_token FROM users WHERE api_token = ? AND id = ?', userToken, userId, function (err, row) {
     if (err) {
       console.log("[DB - ERROR]: ", err);
       return res.status(500).json({ error: "Something Wrong, Try again." });
@@ -230,7 +266,7 @@ app.put("/api/user/:userEmail/mining", (req, res) => {
     if (!row) return res.status(403).json({ error: "Unauthorized Access, Try again." });
 
     // Insert JSON data into the table. 
-    db.run("UPDATE users SET mining_info = ? WHERE api_token = ? AND email = ?", miningInfo, userToken, userEmail, (err) => {
+    db.run("UPDATE users SET mining_info = ? WHERE api_token = ? AND id = ?", miningInfo, userToken, userId, (err) => {
       if (err) {
         console.log("[DB - ERROR]: ", err);
         return res.status(500).json({ error: "Something Wrong With Update, Try again." });
@@ -251,11 +287,11 @@ app.put("/api/user/:userEmail/mining", (req, res) => {
 
 
 
-app.post("/api/withdrawal/approval/:userEmail", (req, res) => {
-  const userEmail = req.params.userEmail;
+app.post("/api/withdrawal/approval/:userId", (req, res) => {
+  const userId = req.params.userId;
 
   // Check if user has VIP/Priviledged withdrawal enabled
-  db.get("SELECT * FROM users WHERE email = ?", [userEmail], (err, row) => {
+  db.get("SELECT * FROM users WHERE id = ?", [userId], (err, row) => {
     if (err) {
       console.log("[DB - ERROR]: ", err);
       return res.status(500).json({ error: "Internal server error" });
@@ -273,68 +309,33 @@ app.post("/api/withdrawal/approval/:userEmail", (req, res) => {
 
 
 app.post("/api/auth", (req, res) => {
-  const { email, password } = req.body;
+  const { email, license } = req.body;
 
-  if (!email || !password) return res.status(400).json({ error: "Credentials Not Valid . . . Try again with valid creds." })
+  if (!license || license.length != 10) return res.status(400).json({ error: "License Key not provided . . . Try again with valid key." })
 
-  const userAPIToken = generateRandomHex();
-  // console.log("[USER-REG]:", email, password, userAPIToken);
-
-  // Check if the user already exists.
-  db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
+  // Check if the license exists.
+  db.get("SELECT * FROM users WHERE license_key = ?", [license], (err, row) => {
     if (err) {
       console.log("[DB-ERROR]:", err);
       return res.status(500).json({ error: "Internal server error" });
     }
 
     if (row) {
-      // User exists, check if approved
+      // License exists, check if license approved
       if (row.approved) {
-        // User is approved, perform login
-        db.get(
-          "SELECT * FROM users WHERE email = ? AND password = ?",
-          [email, password],
-          (loginErr, loginRow) => {
-            if (loginErr) {
-              console.log("[DB-ERROR]:", loginErr);
-              return res.status(500).json({ error: "Internal server error" });
-            }
-
-            // console.log("[LOGIN]: ", loginRow);
-
-            if (!loginRow) {
-              return res.status(401).json({ error: "Invalid credentials" });
-            }
-
-            return res.status(200).json({
-              message: "Login successful",
-              user: { id: loginRow.id, email: loginRow.email, token: loginRow.api_token, miningInfo: JSON.parse(loginRow.mining_info) }
-            });
-          }
-        );
-      } else {
-        // User is not approved
-        return res.status(401).json({ error: "User not approved" });
+        // License is approved, perform login
+        return res.status(200).json({
+          message: "Login successful",
+          user: { id: row.id, licenseKey: row.license_key, email: row.email, token: row.api_token, miningInfo: JSON.parse(row.mining_info) }
+        });
+      }
+      else {
+        // License is not approved 
+        return res.status(401).json({ error: "License/User not activated" });
       }
     } else {
-      // User doesn't exist, perform registration
-      db.run(
-        "INSERT INTO users (email, password, api_token) VALUES (?, ?, ?)",
-        [email, password, userAPIToken],
-        function (registerErr) {
-          if (registerErr) {
-            console.log("[DB-ERROR]:", registerErr);
-            return res.status(500).json({ error: "Failed to register user" });
-          }
-
-          const userId = this.lastID; // Get the last inserted row ID
-
-          return res.status(200).json({
-            message: "User registered successfully click again to login",
-            user: { id: userId, email, token: null },
-          });
-        }
-      );
+      // License key doesn't exist
+      return res.status(500).json({ error: "Unrecognized credentials error" });
     }
   });
 });
